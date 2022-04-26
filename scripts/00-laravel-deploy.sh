1e17029df6dc527d9d5e51316ed668e7c11ae408
Ryan <love99067333@gmail.com>
2022-04-26 17:45:26 +0800

    Update Docker setting

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
-echo "Running composer"
-composer global require hirak/prestissimo
-composer install --no-dev --working-dir=/var/www/html
+/usr/local/bin/docker-php-ext-configure sockets
+/usr/local/bin/docker-php-ext-install sockets
 
 # jetstream
+echo "install jetstream"
 php artisan jetstream:install livewire
 npm install && npm run dev
+
+echo "Running composer"
+composer self-update --2
+composer global require hirak/prestissimo
+composer install --no-dev --working-dir=/var/www/html
 
 echo "Caching config..."
 php artisan config:cache
